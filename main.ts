03679ab9be024e35537ff4095ebfc1fec6ade2e1
BigGSwimmer <128221950+BigGSwimmer@users.noreply.github.com>
2023-03-24 08:41:06 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(1000)
+    basic.pause(500)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(1000)
+    basic.pause(500)
 })
